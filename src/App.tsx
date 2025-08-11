beabf8000b1e51d995714bba0e45bb1ac18ac550
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-08-12 00:23:57 +0600

    installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router";
 import { Button } from "./components/ui/button";
+import CommonLayout from "./components/layout/CommonLayout";
 
 function App() {
     return (
         <>
-            <h1>Hello world</h1>
+            <h1>This is app component</h1>
+            <CommonLayout>
+                <Outlet></Outlet>
+            </CommonLayout>
             <Button>Click Me</Button>
         </>
     );
