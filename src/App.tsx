31ae11c4cba4cbc06954c1c9522cb182e769dee3
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-08-12 22:01:55 +0600

    part 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { Outlet } from "react-router";
-import { Button } from "./components/ui/button";
 import CommonLayout from "./components/layout/CommonLayout";
 
 function App() {
-    return (
-        <>
-            <h1>This is app component</h1>
-            <CommonLayout>
-                <Outlet></Outlet>
-            </CommonLayout>
-            <Button>Click Me</Button>
-        </>
-    );
+  return (
+    <CommonLayout>
+      <Outlet />
+    </CommonLayout>
+  );
 }
 
 export default App;
